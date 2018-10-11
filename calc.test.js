8b35c303110866ad1de15af0bf758f0bd2a7bda6
Steinar Sigurdsson <Steinar@Steinars-MacBook-Pro.local>
2018-10-11 23:08:04 +0000

    step 4

--- calc.test.js
+++ calc.test.js
@@ -16,3 +16,9 @@ test('should return expected sum seperated with new line', () =>{
     expect(add('1, 2 \n 3')).toBe(6);
     expect(add('1\n 2, 3\n 4')).toBe(10);
 });
+
+test('should throw negative numbers', () => {
+    expect(() => add('-2, 3, 5, -8, 7\n 9')).toThrow();
+});
+
+
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,20 +1,32 @@
+
 function add(string){
 
-    //const sep = [',', '\n'];
+    const sep = [',', '\\n'];
+    const neg = [];
 
     if(string.length == 0){
         return 0;
     }
 
 
-    const list = string.split(/[\n,]/);
+    const list = string.split(new RegExp(`[${sep.join('')}]`));
 
     let sum = 0;
     for(let i = 0; i < list.length; i++){
-        sum += parseInt(list[i]);
+        const parsed = parseInt(list[i]);
+        if(parsed < 0){
+            neg.push(parsed);
+        }
+        else{
+            sum += parsed;
+        }
     }
-    return sum;
 
+    if(neg.length > 0){
+        throw `Negatives not allowed: ${negatives.join(', ')}`;
+        }
+
+    return sum;
 }
 
 module.exports = add;
